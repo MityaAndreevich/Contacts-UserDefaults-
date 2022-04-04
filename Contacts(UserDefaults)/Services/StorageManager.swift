6ea217100fab6dd459c29596185c04affbf27ef9
MityaAndreevich <dmitry.logachev@icloud.com>
2022-04-04 15:56:26 +0300

    add possibility of saving with plist

--- Contacts(UserDefaults)/Services/StorageManager.swift
+++ Contacts(UserDefaults)/Services/StorageManager.swift
@@ -12,27 +12,31 @@ class StorageManager {
     
     private let userDefaults = UserDefaults.standard
     private let key = "contacts"
+    private let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    private let archiveURL: URL
     
-    private init() {}
+    private init() {
+        archiveURL = documentDirectory.appendingPathComponent("Contacts").appendingPathExtension("plist")
+    }
     
     func save(contact: Contact) {
         var contacts = fetchContacts()
         contacts.append(contact)
-        guard let data = try? JSONEncoder().encode(contacts) else { return }
-        userDefaults.set(data, forKey: key)
+        guard let data = try? PropertyListEncoder().encode(contacts) else { return }
+        try? data.write(to: archiveURL, options: .noFileProtection)
     }
     
     func fetchContacts() -> [Contact] {
-        guard let data = userDefaults.data(forKey: key) else { return  [] }
-        guard let contacts = try? JSONDecoder().decode([Contact].self, from: data) else { return [] }
+        guard let data = try? Data(contentsOf: archiveURL) else { return  [] }
+        guard let contacts = try? PropertyListDecoder().decode([Contact].self, from: data) else { return [] }
         return contacts
     }
     
     func deleteContact(at index: Int) {
         var contacts = fetchContacts()
         contacts.remove(at: index)
-        guard let data = try? JSONEncoder().encode(contacts) else { return }
-        userDefaults.set(data, forKey: key)
+        guard let data = try? PropertyListEncoder().encode(contacts) else { return }
+        try? data.write(to: archiveURL, options: .noFileProtection)
     }
 }
 
